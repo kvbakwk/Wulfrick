e9b39865521d09f19970b6769c20bf0d1200c5ef
thekubaxd <jakubkawka2005@gmail.com>
2022-10-23 20:41:21 +0200

    Verify

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js')
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js')
 const colors = require('colors')
 const newMsg = require('./logger/newMsg')
 const delMsg = require('./logger/delMsg')
@@ -47,18 +47,23 @@ client.on('messageUpdate', async (msg1, msg2) => {
     editMsg(client, msg1, msg2)
 });
 
-client.on('guildMemberUpdate', async (member) => {
+client.on('guildMemberAdd', async (member) => {
     verifyUser(client, member)
+    console.log(member)
 });
 
 
 client.on('interactionCreate', async i => {
     if (i.isButton()) {
-        if (i.message.embeds[0].footer.text == i.member.user.id) {
+        if (i.customId == i.member.user.id) {
             i.member.roles.remove(client.guilds.cache.get('489087056241229845').roles.cache.get('501127326453465088'))
             i.member.roles.add(client.guilds.cache.get('489087056241229845').roles.cache.get('513438458161922079'))
             i.message.delete()
-            i.deleteReply()
+                .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+                .catch(console.error);
+            i.reply({ content: 'Yeah, you are good', ephemeral: true })
+        } else {
+            i.reply({ content: 'Kliknij może swoją weryfijację, co?', ephemeral: true })
         }
     }
     if (!i.isChatInputCommand()) return;
--- veryfication/verify.js
+++ veryfication/verify.js
@@ -6,11 +6,10 @@ const verifyUser = (c, gMember) => {
         .setAuthor({ name: `Witaj ${gMember.user.username} w Kvba's Community!`, iconURL: `https://cdn.discordapp.com/avatars/${gMember.user.id}/${gMember.user.avatar}.webp?size=128` })
         .setColor('#4F7942')
         .setDescription('Miło Cię tu widzieć! Ale olejmy te uprzejmości. \nJesteś tu, bo wolę nie wpuszczać tutaj jakichś wścibskich botów, wiesz o co chodzi. Kliknij poniższy przycisk, a oszczędzisz sobie tych uprzejmości z mojej strony i dotrzesz tam, dokąd dotrzeć (chyba) chciałeś. Powodzonka.')
-        .setFooter({ text: gMember.user.id })
     const row = new ActionRowBuilder()
         .addComponents(
             new ButtonBuilder()
-                .setCustomId('verify')
+                .setCustomId(gMember.user.id)
                 .setLabel('Zweryfikuj się')
                 .setStyle(ButtonStyle.Success),
         );
